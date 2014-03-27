4b870c1f9cdb07820e6a3592bcdd7f225c9e4c83
mathieu <mathieucans@gmail.com>
2014-03-27 21:21:00 +0100

    initial

---
+++ GameOfLifeReductionism.IntegrationTest/US1_AnyLiveCellWithFewerThanTwoLiveNeighboursDies.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using GameOfLifeReductionism;
+
+namespace GameOfLifeReductionism.IntegrationTest
+{
+    [TestFixture]
+    class US1_AnyLiveCellWithFewerThanTwoLiveNeighboursDies
+    {
+        private GameOfLife Create(string[] init)
+        {
+            var game = new GameOfLife(init);
+            return game;
+        }
+
+        [TestCase]
+        public void Exemple1()
+        {
+            var init = new string[] {
+                "----",
+                "-X--",
+                "----",
+                "----",
+            };
+            var game = Create(init);
+
+            var expected = new string[] {
+                "----",
+                "----",
+                "----",
+                "----",
+            };
+            CollectionAssert.AreEqual(expected, game.Next());
+        }
+
+        [TestCase]
+        public void Exemple2()
+        {
+            var init = new string[] {
+                "----",
+                "-X--",
+                "--X-",
+                "----",
+            };
+            var game = Create(init);
+
+            var expected = new string[] {
+                "----",
+                "----",
+                "----",
+                "----",
+            };
+            CollectionAssert.AreEqual(expected, game.Next());
+        }
+
+       
+    }
+}
---
+++ GameOfLifeReductionism/GameOfLife.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameOfLifeReductionism
+{
+    public class GameOfLife
+    {
+        private string[] panel;
+
+        public GameOfLife(string[] panel)
+        {
+            this.panel = panel;
+        }
+
+        public string[] Next()
+        {
+            return this.panel;
+        }
+    }
+}
